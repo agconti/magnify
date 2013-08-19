d91702d6848aacbdaa46f845b3cf5f211ba5b393
agconti <agc11d@gmail.com>
2013-08-19 15:42:09 -0400

    skeleton search animation

--- js/home_jq.js
+++ js/home_jq.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
 	$("#submit_button").click(function() {
-	  if ($("#search_query").val() != "") {
-	    $("#validate_check").text("Searching...").show();
-	    get_search_results($("#search_query").val());
-	    return true;
-	  }
-	  $("#validate_check").text("Please enter something to search!").show().fadeOut(3000);
-	  return false;
-	  
+		if ($("#search_query").val() != "") {
+			if ($(".result").length	){
+				$(".result").fadeOut(1000);
+				console.log("removed");
+			}
+			var results = get_search_results($("#search_query").val());
+	    	var fadeIn_time = 1000;
+	    	for( i in results){
+	    		var item = $("<p>" + i + "</p>")
+	    		$("#search_results").append(item.hide().fadeIn(fadeIn_time).addClass('result'));
+	    		fadeIn_time += 400;
+	    	return false;
+	    	}
+	   		console.log("querried something");
+	  		} else{
+	  		$("#validate_check").text("Please enter something to search!").show().fadeOut(3000);
+	 		console.log("failed qerry");
+	 		return false;
+			}
 	});
 });
--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 //search.js
 
 var get_search_results = function(term){
-	
+	//set our qeury terms and make query to magnify api
 	var channel_url = 'http://woof.magnify.net/api/';
 	var api_mode = 'content/find?';
 	var search_term = 'vq=' + term;
@@ -9,5 +9,9 @@ var get_search_results = function(term){
 	var api_Key = '&key=84LTHNZQ1364W14D';
 	var return_format = '&format=json';
 	var url = channel_url + api_mode + search_term + return_parms + api_Key + return_format
-	window.open(url, '_self');
+	//var query_results = window.open(url, '_self');
+
+	return url;
+	//parse results
+	//for(item in )
 }
