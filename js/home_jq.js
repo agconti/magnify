f66a3620786c769fcf37660fa627faa66d38aff5
agconti <agc11d@gmail.com>
2013-08-19 21:04:50 -0400

    finished basic version of project

--- js/home_jq.js
+++ js/home_jq.js
@@ -1,25 +1,34 @@
 $(document).ready(function(){
-	$("#submit_button").click(function() {
+	$("#submit_button").click(function(e) {
+		//prevents resubmiting the form and losing the jQuery already inplace.
+		e.preventDefault();
+		//validates query
 		if ($("#search_query").val() != "") {
+			//removes pervious queries, if there are any.
 			if ($(".result").length	){
-				$(".result").each(function(i){
-					this.fadeOut((i*50));});
-				console.log("removed");
+				$("#search_results").fadeOut(1000);
 			}
-			//var results = get_search_results($("#search_query").val());
-	    	//var results = flick();
-	    	var results = data; 
+			//gets url to make a query.
+			var url = get_search_url($("#search_query").val());
+			//makes query
+			$.getJSON(url, function (data) {
+    			results = data;
+  			
+  			//dynamically adds and parses resulting query to page.	
 	    	var fadeIn_time = 1000;
 	    	for(var i=0; i < results.entry.length; i++){
-	    		var item = $("<p>" + results.entry[i].author.name.content + "</p>")
+	    		var title = "<p id='results_title'>"+ results.entry[i].title.content + "</p>" 
+	    		var video = "<iframe id='results_video' src=" + results.entry[i].link[1].href + '/player?layout=&amp;read_more=1 width="420" height="376" frameborder="0" scrolling="no""></iframe>'
+	    		var name = "<p id='author_name'>Video by: " + results.entry[i].author.name.content + "</p>";
+	    		var content = "<p id='results_content'>" + data.entry[i].content.content + "</p>"
+	    		var item = $("<div>" + title + video + name + content + "</div>");
 	    		$("#search_results").append(item.hide().fadeIn(fadeIn_time).addClass('result'));
 	    		fadeIn_time += 400;
 	    	}
-	   		console.log("querried something");
 	   		return	false;
+	   		});
 	  		} else{
 	  		$("#validate_check").text("Please enter something to search!").show().fadeOut(3000);
-	 		console.log("failed qerry");
 	 		return false;
 			}
 	});
--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 //search.js
-function get_search_results(term){
-	//set our qeury terms and make query to magnify api
+
+function get_search_url(term){
+	//set our qeury terms and get a URL for the magnify api
 	
 	var channel_url = 'http://woof.magnify.net/api/';
 	var api_mode = 'content/find?';
@@ -8,16 +9,9 @@ function get_search_results(term){
 	var return_parms = '&per_page=5&page=1&sort=popularity';
 	var api_Key = '&key=84LTHNZQ1364W14D';
 	var return_format = '&format=json';
-	var url = channel_url + api_mode + search_term + return_parms + api_Key + return_format
-	//var query_results = window.open(url, '_self');
-	//var test_list = [1 ,2 ,3, 4, 5, 6, 7, 8, 'im a stringgggg', 9 , 10];
-	//return test_list;
+	var callback = '&callback=?';
+	var url = channel_url + api_mode + search_term + return_parms + api_Key + return_format + callback
+    return url
 
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+	}
 
